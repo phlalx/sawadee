bc35af0f43aa31673e71ea5da053d4c2f9eacbc7
phlalx <phlalx@users.noreply.github.com>
2017-06-29 21:05:00 +0200

    read and extract content of torrent file passed on command line
    put several libs in place (Async, Async.Log, Command and Bencode)

--- main.ml
+++ main.ml
@@ -1,12 +1,48 @@
 open Core
+open Async
+open Log.Global
 
-type t = 
-| Leaf of int
-| Node of t * t
-[@@deriving sexp]
+let some_or_fail x =
+  match x with
+  | Some y -> y
+  | None -> failwith "bad torrent structure"
 
-let tree = Node (Leaf(1), (Leaf(2)))
+let bc_get_infos bc = 
+  let announce_bc = some_or_fail (Bencode.dict_get bc "announce") in
+  let announce_str = some_or_fail (Bencode.as_string announce_bc) in
+  let info_bc = some_or_fail (Bencode.dict_get bc "info") in 
+  let pieces_bc = some_or_fail (Bencode.dict_get info_bc "pieces") in
+  let pieces_str = some_or_fail (Bencode.as_string pieces_bc) in
+  let name_bc = some_or_fail (Bencode.dict_get info_bc "name") in
+  let name_str = some_or_fail (Bencode.as_string name_bc) in 
+  announce_str, pieces_str, name_str
 
-let sexp_tr = sexp_of_t tree 
-let str = Sexp.to_string sexp_tr
-let _ = print_string str
+let do_download f = 
+  let s = In_channel.read_all f in 
+  let bc = Bencode.decode (`String s) in 
+  let s = Bencode.pretty_print bc in
+  let announce, _, name_str = bc_get_infos bc in 
+  string s;
+  string announce;
+  string name_str
+
+let spec =
+  let open Command.Spec in
+  empty
+  +> anon ("FILE" %: string)
+
+let command =
+  Command.basic
+    ~summary:"Download torrent file"
+    spec
+    (fun filename -> (fun () -> (do_download filename)))
+
+let main () = 
+  Command.run command;
+  flushed () >>= fun () ->
+  exit 0
+
+let () = 
+  ignore (main ());
+  never_returns (Scheduler.go ())
+
