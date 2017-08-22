b4d1996a86f4986626a4b5358eb190f5dba297e1
phlalx <phlalx@users.noreply.github.com>
2017-08-23 04:11:57 +0700

    travis script

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,18 +1,13 @@
-OPAM_DEPENDS="ocamlfind oUnit core core_extended ppx_jane uri cohttp async 
-              cohttp-async bencode sha hex"
-
-# async requires ocaml >= 4.03. 
-# we can only get 4.02 from avsm/ppa so we get it from opam
 
 echo "\n" | sudo add-apt-repository ppa:avsm/ppa
 sudo apt-get -y update
 sudo apt-get -y install opam
 
 export OPAMYES=true
-opam init --comp=4.04.2
-opam install ${OPAM_DEPENDS}
-
+opam init --comp=4.05.0
 eval `opam config env`
+opam repo add janestreet-bleeding https://ocaml.janestreet.com/opam-repository
+opam pin add ocaml-torrent .
 
 make all
 make unit
