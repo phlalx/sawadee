7e28ca72ac0e73e101eb3664df7057e39fd99b81
phlalx <phlalx@users.noreply.github.com>
2017-08-22 14:37:07 +0700

    tests more generic
    number of piece is retrieved from input (instead of hard-coded)

--- tests_rpc/bin/test_simple_torrent.ml
+++ tests_rpc/bin/test_simple_torrent.ml
@@ -2,37 +2,35 @@ open Core
 open Async
 open Log.Global
 
+(* TODO make this a ounit test *)
+
 let test file port  =
   let%bind hash = 
     let f = In_channel.read_all file in
     Wrapper.add_torrent f port in
   let%bind () = Clock.after (sec 5.0) in 
   let%bind status = Wrapper.status hash port in 
-  let res = 
-    match status with
-    | Some { torrent = Some { downloaded }} ->  Bitfield.card downloaded 
-    | _ -> assert false 
-  in
-  let%map () = Wrapper.terminate port in
-  res
+  match status with
+  | Some { torrent = Some { tinfo; downloaded }} ->  
+    let num_dl = Bitfield.card downloaded in
+    if not (num_dl = tinfo.Torrent.num_pieces) then
+      failwith "didn't downloaded all the pieces";
+    Wrapper.terminate port 
+  | _ -> failwith "Status not available"
 
 let process port num_clients file () : unit Deferred.t = 
   set_level `Info;
-  let l = List.range port (port + num_clients) in 
-  let%bind res = Deferred.List.map l ~f:(test file) ~how:`Parallel in
-  let s = List.to_string res ~f:string_of_int in
-  info "res = %s" s;
-  flushed () 
-  >>| fun () ->
-  List.iter res ~f:(fun x -> assert (x = 66))
+  List.range port (port + num_clients) 
+  |> Deferred.List.iter ~f:(test file) ~how:`Parallel 
+  >>| fun () -> Print.printf "Test OK\n"
 
 let () = 
   let spec =
     Command.Spec.(
-    empty +> 
-    flag "-r" (required int) ~doc:"set base port"  +>  
-    flag "-n" (required int) ~doc:"set num clients" +>  
-    anon ("URI/FILE" %: string))
+      empty +> 
+      flag "-r" (required int) ~doc:"set base port"  +>  
+      flag "-n" (required int) ~doc:"set num clients" +>  
+      anon ("URI/FILE" %: string))
   in
   let command =
     Command.async ~summary:"Download torrent file" spec process
